6afb46a8d288cc1e25f0e51f94388a16f5f2d3d9
robertdall <robert@robertdall.com>
2012-07-01 11:11:56 -0700

    Styling the footer

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 	<footer>
+			<div id="logo">
+				<h2><a href="<?php echo home_url() ?>/"><?php bloginfo( 'name' ); ?></a></h2>
+			</div>
+
 			<nav id="footer_nav">
 				<?php wp_nav_menu( array( 'container' => 'false', 'theme_location' => 'footer' ) ); ?>
 			</nav>
+			
 	</footer>
 	
 </div><!-- end of wrapper -->
