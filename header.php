f2f9b3b5fa210365b8510b89e1e8ee0a1b11ac04
robertdall <robert@robertdall.com>
2012-07-01 14:46:30 -0700

    Added Logo Header

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/favicon.ico" />
+	
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -17,11 +18,12 @@
 <div id="wrap">
 
 	<header id="branding">
-		<div id="logo"><h2><a href="<?php echo home_url() ?>/"><?php bloginfo( 'name' ); ?></a></h2></div>
+		<div id="logo"><a href="<?php bloginfo('url'); ?>"><img width="240px" src="<?php bloginfo('template_directory'); ?>/images/marquee_main.png" alt="Cult Classics Logo" /></a></div>
 		<nav id="top_nav">
 			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
 		</nav>
 
 	</header>
+
 
 
