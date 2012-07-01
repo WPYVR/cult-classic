a87a9abaf8380c3ac562aa84ee71975a7ea3679e
Joachim Kudish <joachim.kudish@gmail.com>
2012-07-01 11:21:41 -0700

    footer styling adjustments

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
 			<nav id="footer_nav">
 				<?php wp_nav_menu( array( 'container' => 'false', 'theme_location' => 'footer' ) ); ?>
 			</nav>
-			
+
 	</footer>
-	
+
 </div><!-- end of wrapper -->
 <?php wp_footer(); ?>
 </body>
