2dec79230aab4e0b8e0f82e37dcbb912fe75aa45
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2012-07-01 15:49:32 -0700

    Added Google Font Abel, drop shadows main menu

--- functions.php
+++ functions.php
@@ -73,4 +73,17 @@ remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);	
+
+
+function cult_classic_abel_font() {
+	if (!is_admin()) {
+
+		// Enqueue Google Webfonts: Abel
+		wp_register_style( 'cult-classic-abel-style', 'http://fonts.googleapis.com/css?family=Abel', 'all' );
+		wp_enqueue_style( 'cult-classic-abel-style' );
+	}
+}
+
+add_action('init', 'cult_classic_abel_font');
+
 ?>
